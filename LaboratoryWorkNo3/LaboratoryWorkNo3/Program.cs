deaf1469a43c1410dce59218753aef277a90a989
Schtinguerch <schtinguerch@gmail.com>
2023-11-15 20:57:02 +0500

    Laborawory work no 3

---
+++ LaboratoryWorkNo3/LaboratoryWorkNo3/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace LaboratoryWorkNo3
+{
+    class Program
+    {
+        static double BaseFunction(double x)
+        {
+            double value = (Exp(x) - Exp(-x)) / 2;
+            return value;
+        }
+
+        static double FullTaylorSeriesFunction(double x, double epsilon)
+        {
+            if (epsilon <= 0)
+                throw new ArgumentException("Точность не может быть <= 0");
+
+            double value = x;
+            double powerValue = x;
+            long factorialValue = 1;
+
+            var continueCompitung = true;
+
+            for (int i = 1; continueCompitung; i++)
+            {
+                powerValue *= x * x;
+                factorialValue *= 2 * i * (2 * i + 1);
+
+                if (factorialValue == 0)
+                    factorialValue = long.MaxValue;
+
+                double elementValue = powerValue / factorialValue;
+                value += elementValue;
+
+                if (elementValue <= epsilon)
+                    continueCompitung = false;
+            }
+            return value;
+        }
+
+        static double FullTaylorSeriesFunction(double x, int iterationCount)
+        {
+            if (iterationCount <= 0)
+                throw new ArgumentException("Кол-во итераций не может быть <= 0");
+
+            double value = x;
+            double powerValue = x;
+            long factorialValue = 1;
+
+            for (int i = 1; i <= iterationCount; i++)
+            {
+                powerValue *= x * x;
+                factorialValue *= 2 * i * (2 * i + 1);
+
+                if (factorialValue == 0)
+                    factorialValue = long.MaxValue;
+
+                double elementValue = powerValue / factorialValue;
+                value += elementValue;
+            }
+            return value;
+        }
+
+        static void Main(string[] args)
+        {
+            const double StartX = 0.1;
+            const double EndX = 1;
+            const double ComputingAccuracy = 0.000001;
+
+            const int ArgumentCount = 10;
+            const int IterationCount = 20;
+
+            double deltaX = (EndX - StartX) / ArgumentCount;
+
+            WriteLine("Сокращения:\n" +
+                $"Base_Fun - Базовая функция,\n" +
+                $"TS_Iters - Ряд тейлора, кол-во итераций = {IterationCount},\n" +
+                $"TS_Epsil - Ряд тейлора, точность = {ComputingAccuracy},\n" +
+                $"Интервал - [{StartX}; {EndX}]\n");
+
+            for (double x = StartX; x <= EndX; x += deltaX)
+                WriteLine($"   X: {x:N4} =>    " +
+                    $"Base_Fun: {BaseFunction(x):N8};    " +
+                    $"TS_Iters: {FullTaylorSeriesFunction(x, IterationCount):N8}    " +
+                    $"TS_Epsil: {FullTaylorSeriesFunction(x, ComputingAccuracy):N8};" );
+        }
+    }
+}
---
+++ LaboratoryWorkNo3/Test/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int stringLength = 1000000;
+            const int delta = 10000;
+
+            Console.WriteLine("через StringBuilder (Не лагает):\nНажми, чтобы начать!");
+            Console.ReadKey();
+
+            var buider = new StringBuilder("");
+            var stringHolder = "";
+
+            for (int i = 0; i < stringLength; i++)
+            {
+                buider.Append("a");
+
+                if (i % delta == 0)
+                    Console.WriteLine(i + "\tсимволов");
+            }
+
+            Console.WriteLine("\nчерез string (лагает):\nНажми, чтобы начать!");
+            Console.ReadKey();
+
+            for (int i = 0; i < stringLength; i++)
+            {
+                stringHolder += "a";
+
+                if (i % delta == 0)
+                    Console.WriteLine(i + "\tсимволов");
+            }
+        }
+    }
+}
